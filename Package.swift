d51833ffd905c3ea618d169e90a8898617e0bf30
kyungboshim <kyungbo.shim@neowiz.com>
2026-04-06 11:34:22 +0900

    1.0.0 배포

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WithChartBoost",
+    products: [
+        .library(
+            name: "WithChartBoost",
+            targets: ["WithChartBoost"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WithChartBoost",
+            url: "https://github.com/revup-tnk/REVUP-iOS-SDK/releases/download/1.0.0/ChartboostSDK.xcframework.zip",
+            checksum: "21ab73dc8f95f325bc31b63e66a64c1d719ae306d8c4c50e08d494200f86dfdd"
+        ),
+    ]
+)
